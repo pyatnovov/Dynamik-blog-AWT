0e40e43fd39d27c0fcda233cf2464dc6378a8f68
Volodymyr Piatnytskyi <volodymyrpiatnytskyi@Mac1234.local>
2024-12-14 12:43:19 +0100

    fix

--- js/index.js
+++ js/index.js
@@ -5,7 +5,18 @@ import DropdownMenuControl from "./dropdownMenuControl.js";
 
 window.drMenuCntrl = new DropdownMenuControl("menuIts", "menuTitle", "mnShow");
 
-window.router = new Router(Routes,"welcome");
+window.router = new Router(Routes, "welcome");
+
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Event delegation: Listen for clicks on any element with the 'back-button' class
+  document.addEventListener('click', function (e) {
+    if (e.target.closest('.back-button')) {
+      e.preventDefault(); // Prevent the default anchor behavior
+      window.history.back(); // Navigate to the previous page in history
+    }
+  });
+});
 
 
 
